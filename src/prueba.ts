212e25bc0bdd153cc166d72ad8330884f35b9658
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-05-01 00:30:40 +0100

    Coverage updated and prueba updated

--- src/prueba.ts
+++ src/prueba.ts
@@ -1,9 +1,3 @@
-/**
- * Método para hacer una suma
- * @param a primer numero
- * @param b segundo numero
- * @returns suma de los numeros
- */
 export function add(a: number, b: number): number {
   return a + b;
 }
